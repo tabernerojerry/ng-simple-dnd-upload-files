ba61b943cb93dfaf80c329a132ee3a1678ee319e
Jerry Tabernero <jerry_tabernero+668899@users.noreply.github.com>
2024-04-14 23:32:57 +0800

    drap and drop files upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,69 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HotToastService } from '@ngxpert/hot-toast';
+import { serialize } from 'object-to-formdata';
+
+const ALLOWED_FILE_TYPES = [
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'simple-dnd-upload-files';
+  @ViewChild('fileInput', { static: false }) fileInput!: ElementRef;
+
+  #hotToastService = inject(HotToastService);
+
+  allowedFileTypes = ALLOWED_FILE_TYPES;
+
+  isUploading = false;
+  fileUrl!: string | null;
+  uploadFile!: File | null;
+
+  handleChange(event: any) {
+    /**
+     * To simulate the upload
+     * temporary update the isUploading value to false
+     * this should be done the updates in the API call
+     */
+    this.isUploading = false;
+
+    const file = event.target.files[0] as File;
+
+    if (this.allowedFileTypes.indexOf(file?.type) === -1) {
+      this.#hotToastService.error('File type is not allowed.');
+      this.handleRemovesFile();
+      return;
+    }
+
+    this.fileUrl = URL.createObjectURL(file);
+    this.uploadFile = file;
+  }
+
+  handleRemovesFile() {
+    if (this.fileInput && this.fileInput.nativeElement) {
+      this.fileInput.nativeElement.value = null;
+    }
+
+    this.uploadFile = null;
+    this.fileUrl = null;
+  }
+
+  handleUploadFile() {
+    this.isUploading = true;
+
+    const formData = serialize({
+      document: this.uploadFile
+    });
+
+    // your api service logic to upload file
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,14 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHotToastConfig } from '@ngxpert/hot-toast';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes), 
+    provideHotToastConfig({
+      dismissible: true,
+      duration: 5000,
+    })
+  ]
 };
